b936c8a000353b156b88e4cec3204021613cf0d5
FERNANDO SOARES DE ALBUQUERQUE <fer.albuquerque90@gmail.com>
2017-09-10 20:35:25 -0300

    Adicionadas as propriedades da classe Receita

--- ionit-receitas.core/Entities/Receita.cs
+++ ionit-receitas.core/Entities/Receita.cs
@@ -13,6 +13,9 @@
         /// Gets or sets the id.
         /// </summary>
         public Int32 Id { get; set; }
+        public string Titulo { get; set; }
+        public string Ingredientes { get; set; }
+        public string ModoPreparo { get; set; }
         
         #endregion
     }
