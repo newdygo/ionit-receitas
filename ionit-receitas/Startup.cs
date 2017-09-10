94aee88120b8b247fbf454508f6837f252f40e1f
Diego Leonardo Balduino <diego.balduino@conductor.com.br>
2017-09-10 20:19:26 -0300

    Inclusão da dependência da camadas core com o projeto de visualização e api

--- ionit-receitas/Startup.cs
+++ ionit-receitas/Startup.cs
@@ -27,6 +27,8 @@ namespace ionit_receitas
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
+
             // Add framework services.
             services.AddMvc();
         }
