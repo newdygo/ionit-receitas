1299fa701d55b809741fd9ace0a96f5fe2771f4f
Diego Balduino <homertrix_x@hotmail.com>
2017-09-11 20:14:35 -0300

    Incluindo o design context na versão antiga

--- ionit-receitas.core/Context/DesignTimeContext.cs
+++ ionit-receitas.core/Context/DesignTimeContext.cs
@@ -1,27 +1,26 @@
 ﻿namespace ionit.receitas.core.Context
 {
-    //using ionit.receitas.core.Config;
-    //using Microsoft.EntityFrameworkCore;
-    //using Microsoft.EntityFrameworkCore.Design;
-    //using Microsoft.Extensions.Configuration;
-    //using Microsoft.Extensions.Options;
-    //using System;
+    using ionit.receitas.core.Config;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Infrastructure;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Options;
 
-    //public class DesignTimeContext : IDesignTimeDbContextFactory<ReceitaContext>
-    //{
-    //    public ReceitaContext CreateDbContext(string[] args)
-    //    {
-    //        var builder = new DbContextOptionsBuilder<ReceitaContext>();
+    public class DesignTimeContext : IDbContextFactory<ReceitaContext>
+    {
+        public ReceitaContext Create(DbContextFactoryOptions options)
+        {
+            var builder = new DbContextOptionsBuilder<ReceitaContext>();
 
-    //        IConfigurationRoot configuration = new ConfigurationBuilder()
-    //          .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
-    //          .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-    //          .Build();
-            
-    //        var appOption = new AppOption();
-    //        configuration.GetSection("AppOptions").Bind(appOption);
-            
-    //        return new ReceitaContext(Options.Create(appOption));
-    //    }
-    //}
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+              .SetBasePath(options.ApplicationBasePath)
+              .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+              .Build();
+
+            var appOption = new AppOption();
+            configuration.GetSection("AppOptions").Bind(appOption);
+
+            return new ReceitaContext(Options.Create(appOption));
+        }
+    }
 }
